5ed5798ed7aa19a7bfc3c8bb6edc0e961544eee8
alexzamurca <alexzamurca@yahoo.co.uk>
2021-03-12 11:10:11 +0000

    completed Week 4: 4A exercise 1, removeAll may not be done the way they wanted me to do it. Need to mark it.

--- Week 4/4A-List comprehension.hs
+++ Week 4/4A-List comprehension.hs
@@ -3,34 +3,61 @@
 ------------------------- Exercise 1
 
 doubles :: [Int] -> [Int]
-doubles = undefined
+doubles [] = []
+doubles xs = [(* 2) x | x <- xs]
 
 odds :: [Int] -> [Int]
-odds = undefined
+odds [] = []
+odds xs = [x | x <- xs, odd x]
 
 doubleodds :: [Int] -> [Int]
-doubleodds = undefined
+doubleodds [] = []
+doubleodds xs = [(* 2) x | x <- xs, odd x]
 
 shorts :: [String] -> [String]
-shorts = undefined
+shorts [] = []
+shorts xs = [x | x <- xs, islengthlessthan6 x]
+    where 
+            islengthlessthan6 :: String -> Bool
+            islengthlessthan6 x = length x < 6
 
 squarePositives :: [Int] -> [Int]
-squarePositives = undefined
+squarePositives [] = []
+squarePositives xs = [(^ 2) x | x <- xs, ispositive x]
+    where 
+        ispositive :: Int -> Bool
+        ispositive x = x > 0
 
 oddLengthSums :: [[Int]] -> [Int]
-oddLengthSums = undefined
+oddLengthSums [] = []
+oddLengthSums xs = [sum x| x <- xs, oddlength x]
+    where
+        oddlength :: [Int] -> Bool
+        oddlength [] = False 
+        oddlength xs = odd (length xs)
 
 remove :: Eq a => [a] -> a -> [a]
-remove = undefined
+remove [] _ = []
+remove xs y = [x | x <- xs, (noty y) x]
+    where 
+        noty :: Eq a => a -> a -> Bool
+        noty x y = x /= y
 
 removeAll :: Eq a => [a] -> [a] -> [a]
-removeAll = undefined
+removeAll xs [] = xs
+removeAll [] _ = []
+removeAll xs (y: ys) = removeAll (remove xs y) ys 
 
 everyother :: [a] -> [a]
-everyother = undefined
+everyother [] = []
+everyother xs = [snd x | x <- zip [1..] xs, p x]
+    where
+        p(x , _) =  odd x
 
 same :: Eq a => [a] -> [a] -> [Int]
-same = undefined
+same xs ys =  [fst x | x <- (zip [1..] (zipWith (==) xs ys)), p x]
+    where
+        p(_, y) = y
 
 
 ------------------------- Exercise 2
