52b70f6aad3efb4d42f1e70c47e358fb9786235f
alexzamurca <alexzamurca@yahoo.co.uk>
2021-03-23 20:05:54 +0000

    completed 5A

--- Week 5/5A-Trees.hs
+++ Week 5/5A-Trees.hs
@@ -9,23 +9,50 @@ t = Node 4 (Node 2 (Node 1 Empty Empty) (Node 3 Empty Empty)) (Node 5 Empty (Nod
 ------------------------- Exercise 1
 
 isEmpty :: IntTree -> Bool
-isEmpty Empty = undefined
-isEmpty _     = undefined
+isEmpty Empty = True
+isEmpty _     = False
 
 rootValue :: IntTree -> Int
-rootValue Empty        = undefined
-rootValue (Node i _ _) = undefined
+rootValue Empty        = 0
+rootValue (Node i _ _) = i
 
 height :: IntTree -> Int
-height = undefined
+height Empty = 0
+height (Node n lnode rnode) = 1 + max (height lnode) (height rnode)
+  where 
+    max :: Int -> Int -> Int
+    max i1 i2
+      | i1 == 12 = i1
+      | i1 > i2 = i1
+      | otherwise = i2
 
 member :: Int -> IntTree -> Bool
-member = undefined
+member _ Empty = False
+member i (Node n lnode rnode)
+  | i==n = True 
+  | i < n = member i lnode
+  | otherwise = member i rnode
 
 paths :: Int -> IntTree -> [[Int]]
-paths = undefined
+paths _ Empty = []
+paths i (Node n lnode rnode) = path i (Node n lnode rnode) : (0 : path i lnode) : (1 : path i rnode) : [[]]
+  
+path :: Int -> IntTree -> [Int]
+path _ Empty = []
+path i tree
+  | member i tree = getpath i tree
+  | otherwise = []
+  where
+    getpath :: Int -> IntTree -> [Int]
+    getpath i (Node n lnode rnode)
+      | i == n = []
+      | i < n = 0 : getpath i lnode
+      | otherwise = 1 : getpath i rnode 
 
--------------------------
+    
+  
+
+
 
 {-
 instance Show IntTree where
@@ -36,45 +63,55 @@ instance Show IntTree where
           [ c:' ':m | m <- aux ' ' '|' s ] ++ 
           ['+':'-':show x] ++ 
           [ d:' ':n | n <- aux '|' ' ' t ]
--}
 
+-}
 
 
 ------------------------- Exercise 2
 
-type Var = ()
+type Var = String
 
-data Term
---  deriving Show
+data Term = 
+  Variable Var 
+  | Lambda Var Term 
+  | Apply Term Term
+  -- deriving Show
 
-{-
+
 example :: Term
 example = Lambda "a" (Lambda "x" (Apply (Apply (Lambda "y" (Apply (Variable "a") (Variable "c"))) (Variable "x")) (Variable "b")))
--}
 
-{-
+
 pretty :: Term -> String
 pretty = f 0
     where
       f i (Variable x) = x
       f i (Lambda x m) = if i /= 0 then "(" ++ s ++ ")" else s where s = "\\" ++ x ++ ". " ++ f 0 m 
       f i (Apply  n m) = if i == 2 then "(" ++ s ++ ")" else s where s = f 1 n ++ " " ++ f 2 m
--}
+
+
+instance Show Term where
+  show = pretty
+
 
 n1 :: Term
-n1 = undefined
+n1 = Lambda "x" (Variable "x") 
 
 n2 :: Term
-n2 = undefined
+n2 = Lambda "x" (Apply (Lambda "y" (Variable "x")) (Variable "z"))
 
 n3 :: Term
-n3 = undefined
+n3 = Apply (Lambda "x" (Lambda "y" (Apply (Variable "x") (Variable "y")))) n1
 
 used :: Term -> [Var]
-used = undefined
+used (Variable x) = [x]
+used (Lambda x m) =  [x] `merge` used m
+used (Apply n m) = used n `merge` used m
 
 free :: Term -> [Var]
-free = undefined
+free (Variable x) = [x]
+free (Lambda x m) = free m `minus` [x]
+free (Apply n m) = free n `merge` free m
 
 
 
